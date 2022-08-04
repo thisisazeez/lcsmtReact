cc08cefa8a0465ae9f9d70c41235faed82274192
thisisazeez <imfostersyndrome@gmail.com>
2022-08-04 06:03:51 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -27,22 +27,24 @@ import Bike from './components/Bike'
 
 
 function App() {
-  const bikes = ['Suzuki', 'BMW', 'Benz'] // list = bikes data = stored in the list `bikes`
-  // const bikes = [
-  //   {id:1, brand:'Ford'},
-  //   {id:2, brand:'Suzuki'},
-  //   {id:3, brand:'BMW'}
-  // ]
+  // const bikes = ['Suzuki', 'BMW', 'Benz'] // list = bikes data = stored in the list `bikes`
+  const bikes = [
+    {id:1, brand:'Ford'},
+    {id:2, brand:'Suzuki'},
+    {id:3, brand:'BMW'},
+    {id:4, brand:'Ducati'}
+  ]
+  console.log(bikes)
 return(
     <div className="App">
 
       <h3>How many bikes do we have ?</h3>
-      <ul>
-        {bikes.map((bikes) => <Bike brand={bikes}/>)} {/* rendering the list to our bike component */}
-      </ul>
       {/* <ul>
-        {bikes.map((bikes) => <Bike key={bikes.id} brand={bikes.brand}/>)}
+        {bikes.map((bikes) => <Bike brand={bikes}/>)} rendering the list to our bike component
       </ul> */}
+      <ul>
+        {bikes.map((bikes) => <Bike key={bikes.id} brand={bikes.brand}/>)}
+      </ul>
       <hr/>
       {/* <Either isGood={true}/> */}
     </div>
